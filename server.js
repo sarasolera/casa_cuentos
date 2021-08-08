161f35447e7ff79b29f60fc3ef66062c24ed6078
Sara Solera <sarasolera@correo.ugr.es>
2021-08-08 19:23:17 +0200

    Modificado el servidor para añadir la escucha de socket y crear la instancia room

--- server.js
+++ server.js
@@ -6,10 +6,25 @@ var io = require("socket.io")(server);
 app.use(express.static("public"));
 app.use(express.static("views"));
 
-//Ponemos el servidor a escuchar
+/**
+ * Importamos la clase room y creamos una instancia de la sala
+ */
+const Room = require("./room");
+var r;
+//Creamos la sala
+r = new Room();
 
+//socket escuchando conexiones
 io.on("connection", function(socket){
+    //Peticion de un cliente de unirse al juego
+    socket.on("joinRoom", joinRoom);
 
+    function joinRoom(name_player){
+      console.log("Se ha unido un jugador nuevo llamado: ",name_player);
+      r.addPlayer(name_player);
+      //Enviamos la url
+      socket.emit("room",r.url);
+    }
 });
 
 //Ponemos al servidor a escuchar por el puerto 80
