64b8a1fc46faa619e6fd232c11380a5020c97c05
Sara Solera <sarasolera@correo.ugr.es>
2021-09-28 16:39:26 +0200

    Añadiendo estilo y funciones en el cliente para la fase 2 #21

--- public/client.js
+++ public/client.js
@@ -138,36 +138,127 @@ function chooseLandscape(carta) {
 
 socket.on("showCard", showCard);
 
-function showCard(card) {
-  //Volvemos al tablero
+
+function returnBoard(){
   $('#bloque_central').addClass('hidden');
   $("#bloque_tablero").removeClass('hidden');
-  $("#bloque_tablero").load("./board.html");
+  //$("#bloque_tablero").load("./board.html");
+}
 
-  cadena =
-    "<p>Carta paisaje seleccionada:</p><img id=carta_recorrido src=assets/images/landscape/" +
-    card +
-    " >";
+function showCard(card) {
+  //Volvemos al tablero
+  returnBoard();
+  cadena ="<p>Carta paisaje seleccionada:</p><img id=carta_recorrido src=assets/images/landscape/" +card +" >";
   $("#bloque_recorrido").append(cadena);
   console.log("Cerramos puerta ");
-  socket.emit("close_door",0);
-
+  socket.emit("close_door",0,1);
 }
 
 socket.on("close_door",closeDoor);
 
 
-function closeDoor(index){ 
-  console.log("Puerta superada");
+function closeDoor(index,next){ 
+  console.log("#puerta_"+index)
   //Cambiamos la puerta en la que hemos entrado
-  $("#puerta_uno").attr('src','/assets/images/estrella.png');  
-  $("#puerta_uno").removeClass("abierta");
-  $("#puerta_uno").addClass("cerrada");
-  $("#texto_puerta_uno").addClass("hidden");
-  
-//Cambiamos la puerta en la que vamos a entrar
-  $("#puerta_dos").attr('src','/assets/images/libro_abierto.png');  
-  $("#puerta_dos").removeClass("cerrada");
-  $("#puerta_dos").addClass("abierta");
-  $("#texto_puerta_dos").removeClass("hidden");
+  $('#puerta_'+index).attr('src','/assets/images/estrella.png');  
+  $('#puerta_'+index).addClass("estrella");
+  $("#puerta_"+index   ).removeClass("abierta");
+  $('#texto_puerta_'+index).addClass("hidden");
+
+  //Cambiamos la puerta en la que vamos a entrar
+  $("#puerta_"+next).attr('src','/assets/images/libro_abierto.png');  
+  $("#puerta_"+next).addClass("abierta");
+  $("#puerta_"+next).removeClass("cerrada");
+  $("#texto_puerta_"+next).removeClass("hidden");
+
 };
+
+//Obtenemos genero y tramas
+function getGender(){
+  //Pedimos al servidor uin genero y 3 tramas
+  socket.emit("getGender");
+}
+
+socket.on("showGender",showGender);
+
+var gender_selected = null;
+
+function showGender(gender){
+  console.log("Entro en showGender");
+  cadena = "<p>Genero de la historia: </p><p id=nombre_genero>"+gender+"</p>";
+  $('#bloque_genero').append(cadena);
+  $('#bloque_genero').removeClass('hidden');
+  gender_selected = gender;
+  getPlots();
+
+}
+
+function getPlots(){
+  socket.emit("getPlots");
+}
+
+socket.on("showPlots" , showPlots);
+
+var plots_selected = [];
+function showPlots(plots){
+  console.log("Entro en showPLots");
+  plots_selected = plots;
+
+  cadena="<div id=bloque_tramas>"
+  for(var i = 0 ; i < 3 ; i++){
+    cadena+='<div class=tramas><p>'+plots[i]+'</p> <button type=button onclick=getDescription('+i+')>Explicación</button>'+'</div>';
+  }
+  cadena+="</div>"
+
+  $("#trama_genero").append(cadena);
+
+  $("#b_genero").addClass("hidden");
+
+  $("#bloque_fase").append('<button id="continuar" type=button onclick=showSelectedGender()> Continuar...</button>');
+  
+}
+
+function getDescription(index){
+  socket.emit("getDescription",index);
+}
+
+
+socket.on("showDescription" , showDescription);
+
+function showDescription(description){
+  console.log($("#descripcion"));
+  if($("#descripcion").length > 0 ){
+    console.log("if")
+    
+    $("#descripcion").text(description);
+  }
+  else{
+    console.log("else")
+    cadena="<div id=descripcion>";
+    cadena+='<p id=texto_descripcion>'+description+'</p>';
+    cadena+="</div>";
+    $("#trama_genero").append(cadena);
+  }
+
+}
+
+function showSelectedGender(){
+  socket.emit("showSelectedGender" , showSelectedGender);
+  
+}
+
+socket.on("showSelectedGender",function(){
+  returnBoard();
+  console.log(gender_selected);
+  console.log(plots_selected);
+  cadena ="<p>Genero => " +gender_selected+"</p>";
+  cadena+="<p>Tramas: </p>";
+
+  plots_selected.forEach(element=>{
+    cadena += "<p>"+element+"</p>";
+  })
+
+  $("#bloque_recorrido").append(cadena);
+
+  socket.emit("close_door",1,2);
+})
